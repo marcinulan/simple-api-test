dec00e36a5f669e9c7773727c19f6188c4fb3f8b
Marcin Ułanowicz <ulan@pop-ulan-pc.home>
2020-01-21 18:58:27 +0100

    Add assertion after adding the favorite

--- src/exerciseTest.java
+++ src/exerciseTest.java
@@ -1,21 +1,24 @@
 import io.restassured.http.ContentType;
-import io.restassured.response.ResponseBody;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Random;
 
 import static io.restassured.RestAssured.given;
-
+import static org.hamcrest.Matchers.equalTo;
 
 public class exerciseTest {
-    String mainUri = "https://api.thecatapi.com/v1";
-    String apiKey = "61dd1797-9ea8-443e-94cf-265fefd9e56f";
-    Random rand = new Random();
-    int favId = rand.nextInt(99999);
+    static String mainUri = "https://api.thecatapi.com/v1";
+    static String apiKey = "61dd1797-9ea8-443e-94cf-265fefd9e56f";
+    static Random rand = new Random();
+    static String favId = String.valueOf(rand.nextInt(99999));
+    static HashMap postBody = getPostBody();
+    static String favImageId = postBody.get("image_id").toString();
+    static Integer favMainId;
 
-    String getImageId() {
-        ResponseBody response = given().
+
+    public static HashMap getPostBody() {
+        var response = given().
                         param("size", "small").
                         param("limit", "1").
                         param("page", "33").
@@ -25,9 +28,12 @@ public class exerciseTest {
                         get(mainUri + "/images/search").
                     then().
                         statusCode(200).
-                        contentType(ContentType.JSON).extract().response().body();
-        String imageId = response.jsonPath().get("id[0]");
-        return imageId;
+                        contentType(ContentType.JSON);
+        String imageId = response.extract().response().body().jsonPath().get("id[0]");
+        HashMap<String, String> map = new HashMap();
+        map.put("image_id", imageId);
+        map.put("sub_id", String.valueOf(favId));
+        return map;
     }
 
     @Test
@@ -42,17 +48,16 @@ public class exerciseTest {
 
     @Test
     public void setFav() {
-        HashMap<String, String> map = new HashMap();
-        map.put("image_id", getImageId());
-        map.put("sub_id", String.valueOf(favId));
-        given().
+        var response = given().
                 contentType("application/json").
                 header("x-api-key", apiKey).
-                body(map).
+                body(postBody).
         when().
                 post(mainUri + "/favourites").
         then().
-                statusCode(200);
+                statusCode(200).
+                contentType(ContentType.JSON);
+        favMainId = response.extract().response().body().jsonPath().get("id");
     }
 
     @Test
@@ -60,8 +65,12 @@ public class exerciseTest {
         given().
                 header("x-api-key", apiKey).
         when().
-                get(mainUri + "/favourites/" + favId).
+                get(mainUri + "/favourites/" + favMainId).
         then().
-                statusCode(200);
+                statusCode(200).
+                body("sub_id", equalTo(favId)).
+                body("id", equalTo(favMainId)).
+                body("image_id", equalTo(favImageId));
+
     }
 }
